ee2b67c2b8acf9924f8d92bb23d35bd429a37b18
3nan3 <nom3nan@gmail.com>
2015-06-23 10:15:25 +0900

    copy

---
+++ 3nan3/web-server/app.php
@@ -0,0 +1,38 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$app = new My1DayServer\Application();
+$app['debug'] = true;
+
+$app->get('/messages', function () use ($app) {
+    $messages = $app->getAllMessages();
+
+    return $app->json($messages);
+});
+
+$app->get('/messages/{id}', function ($id) use ($app) {
+    $message = $app->getMessage($id);
+
+    return $app->json($message);
+});
+
+$app->post('/messages', function (Request $request) use ($app) {
+    $data = $app->validateRequestAsJson($request);
+
+    $username = isset($data['username']) ? $data['username'] : '';
+    $body = isset($data['body']) ? $data['body'] : '';
+
+    $createdMessage = $app->createMessage($username, $body, base64_encode(file_get_contents($app['icon_image_path'])));
+
+    return $app->json($createdMessage);
+});
+
+$app->delete('/messages/{id}', function ($id) use ($app) {
+    $app->deleteMessage($id);
+
+    return new Response('', Response::HTTP_NO_CONTENT);
+});
+
+return $app;
