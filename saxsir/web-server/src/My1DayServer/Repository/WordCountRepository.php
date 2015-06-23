99e70f57a5e937cd4ed385e4e9484e453bae82bd
saxsir <saxsir.256+github@gmail.com>
2015-06-23 15:42:34 +0900

    'hoge'という単語をカウントアップする機能を追加

--- saxsir/web-server/src/My1DayServer/Repository/WordCountRepository.php
+++ saxsir/web-server/src/My1DayServer/Repository/WordCountRepository.php
@@ -15,5 +15,22 @@ class WordCountRepository
 
     // wordが既に登録済みか判定する
 
-    // 受け取った単語をカウントアップする
+    /**
+     * 受け取った単語をカウントアップする
+     *
+     * @param word [String] カウントアップする単語
+     * @return count [Int] カウントアップした後の値
+     */
+    public function increment($word) {
+      $sql = 'SELECT count FROM word_count WHERE word = ?';
+      $params = [$word];
+      $count = $this->conn->fetchAssoc($sql, $params)['count'];
+
+      //HACK: idで指定した方が高速だが、そこまで求められていないので今回は対応しない
+      $sql = 'UPDATE word_count SET count = ? WHERE word = ?';
+      $count = $count + 1;
+      $params = [$count, $word];
+      $this->conn->executeUpdate($sql, $params);
+      return $count;
+    }
 }
