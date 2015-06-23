2d78c0c9f335c95ebda4ebcc1aed2d3522eb4f50
saxsir <saxsir.256+github@gmail.com>
2015-06-23 10:07:57 +0900

    [wip] connect storyboard and MessageTableViewCell

--- saxsir/iOS-client/My1DayApp/MessageTableViewCell.swift
+++ saxsir/iOS-client/My1DayApp/MessageTableViewCell.swift
@@ -12,11 +12,13 @@ class MessageTableViewCell: UITableViewCell {
     @IBOutlet weak private var iconImageView: UIImageView!
     @IBOutlet weak private var messageLabel: UILabel!
     // Mission1-1 UILabel のインスタンス変数を追加
+    @IBOutlet weak var createdAtLabel: UILabel!
     
     override func prepareForReuse() {
         self.iconImageView.image = nil
         self.messageLabel.text = nil
         // Mission1-1 UILabel のインスタンス変数を初期化
+        self.createdAtLabel.text = nil
     }
     
     func setupComponentsWithMessage(message: Message) {
